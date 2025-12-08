7fd2ecf6067dfec9be0b9e60aaa4365daa8f5296
matthewchavis8 <matthewchavis8@gmail.com>
2025-12-08 12:21:44 -0600

    Flashed a Pin  - messed around with controlling pins

--- Arduino/main.cpp
+++ Arduino/main.cpp
@@ -1,12 +1,18 @@
 #include <avr/io.h>
+#include <stdint.h>
 #include <util/delay.h>
 
-int main() {
-
-  DDRB |= (1<<PB5);
+void flashPin(uint8_t pin) {
+  DDRB |= (1 << pin);
 
   for (;;) {
-    PORTB ^= (1 << PORTB5);
+    // PORT is hard coded this is mapped to some MMIO
+    PORTB ^= (1 << pin);
     _delay_ms(1000);
   }
+}
+
+int main() {
+  // Setting the Data Direction Register for PORT b pin 5 to output
+  flashPin(PB3);
 }
