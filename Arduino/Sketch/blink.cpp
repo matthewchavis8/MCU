ae7ed18fbe814eccc16a77f6a2df6c63cf456b7c
matthewchavis8 <matthewchavis8@gmail.com>
2025-12-13 03:03:01 -0600

    Edited Blink.cpp

--- Arduino/Sketch/blink.cpp
+++ Arduino/Sketch/blink.cpp
@@ -1,18 +1,13 @@
 #include <avr/io.h>
-#include <stdint.h>
 #include <util/delay.h>
 
-void flashPin(uint8_t pin) {
-  DDRB |= (1 << pin);
-
-  for (;;) {
-    // PORT is hard coded this is mapped to some MMIO
-    PORTB ^= (1 << pin);
-    _delay_ms(1000);
-  }
-}
 
 int main() {
-  // Setting the Data Direction Register for PORT b pin 5 to output
-  flashPin(PB3);
+  // Setting the Data Direction Register for PORT b pin 0 to output
+  DDRB |= (1 << DDB0);
+
+  for (;;) {
+    PORTB ^= (1 << PB0);
+    _delay_ms(1000);
+  }
 }
