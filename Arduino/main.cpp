d6239d15743b762ef0fbe34d7fea0f5e7e5e11a3
matthewchavis8 <matthewchavis8@gmail.com>
2025-12-16 21:24:45 -0600

    Build System Change and Update:  - In a nutshell F makefiles back to CMake  - Moved the module folder up one and removed sketch  - Moved independent programs into an Independent Folder  - Set up toolchain for CMake  - Set up flashing and linked the toolchain

--- Arduino/main.cpp
+++ Arduino/main.cpp
@@ -1,4 +1,4 @@
-#include "Sketch/Modules/UART/UART.h"
+#include "Modules/UART/UART.h"
 #include <util/delay.h>
 
 int main() {
@@ -6,7 +6,7 @@ int main() {
 
   Uart uart(115200);
   while (1) {
-    uart.print("Your mom is gay\n");
+    uart.print("LOLLLL IS IT WORKING????\n");
     _delay_ms(1000);
   }
 }
