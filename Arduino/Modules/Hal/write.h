7489316455e1a611105624384d64e2c8fffc242b
matthewchavis8 <matthewchavis8@gmail.com>
2025-12-20 03:02:51 -0600

    Added Digital Read

--- Arduino/Modules/Hal/write.h
+++ Arduino/Modules/Hal/write.h
@@ -3,11 +3,12 @@
 
 #include <avr/io.h>
 
-static inline void digitalWrite(const uint8_t pin, bool mode) {
-  if (mode)
-    PORTB |= (1 << pin);
-  else 
-    PORTB &= ~(1 << pin);
+static inline void digitalWrite(const uint8_t pin, bool isHigh) noexcept {
+  isHigh ? PORTB |= (1 << pin) : PORTB &= ~(1 << pin);
+}
+
+[[nodiscard]] static inline bool digitalRead(uint8_t pin) noexcept {
+  return (PINB & (1 << pin)) != 1;
 }
 
 #endif // !__WRITE_H__
--- Arduino/main.cpp
+++ Arduino/main.cpp
@@ -3,18 +3,30 @@
 #include <util/delay.h>
 #include "Modules/Hal/write.h"
 
-
 int main() {
-  constexpr uint8_t ledPins[] = { PB0, PB1, PB2, PB3, PB4, PB5 };
+   
+  // Flashing Multiple LED
+  // Turning Pins to Output
+  constexpr uint8_t ledPins[] = { PB1, PB2, PB3, PB4, PB5 };
   for (auto pin : ledPins)
     DDRB |= (1 << pin);
 
-  for (;;) {
-    for (auto pin : ledPins) {
-      digitalWrite(pin, true);
-      _delay_ms(100);
-      digitalWrite(pin, false);
-      _delay_ms(100);
+  constexpr uint8_t btnPin = PB0;
+  PORTB |= (1 << btnPin);
+  
+  while (true) {
+    bool btnState = digitalRead(PB0);
+    
+    if (btnState) {
+      for (auto pin : ledPins) {
+
+        digitalWrite(pin, true);
+        _delay_ms(30);
+
+        digitalWrite(pin, false);
+        _delay_ms(30);
+      }
     }
+    _delay_ms(5);
   }
 }
